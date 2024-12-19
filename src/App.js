e0e5b3cd3d2c4466293755a51d57507648e4f387
Harsha Sheshanna <harsha.sheshanna@gmail.com>
2024-12-18 19:24:35 -0500

    intial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = () => {
+    if (newTodo.trim()) {
+      setTodos([...todos, newTodo]);
+      setNewTodo('');
+    }
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>React To-Do App</h1>
+        <div>
+          <input
+            type="text"
+            value={newTodo}
+            onChange={(e) => setNewTodo(e.target.value)}
+            placeholder="Add a new task"
+          />
+          <button onClick={addTodo}>Add</button>
+        </div>
+        <ul>
+          {todos.map((todo, index) => (
+            <li key={index}>
+              {todo} <button onClick={() => removeTodo(index)}>Remove</button>
+            </li>
+          ))}
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
